29dec5f3aa5d8194fda2fbf3921a260b6037ba9b
vivek vardhan . M <84318895+vikkyvivek2002@users.noreply.github.com>
2021-07-04 09:44:02 +0530

    Add files via upload

---
+++ ConstructorExample.java
@@ -0,0 +1,14 @@
+class Sample
+{ 
+Sample()
+{
+System.out.println("This is constructor");
+} 
+}
+class ConstructorExample
+{ 
+public static void main(String args[])
+{ 
+Sample obj=new Sample(); 
+}
+}
